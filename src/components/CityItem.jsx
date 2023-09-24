736c08784c3736545521ca36e47e1e614a53415c
Mahdi Pasandideh <mtipsd.git@gmail.com>
2023-09-24 16:25:33 +0330

    learned :: useSearchParams we can use this hook to have a global state

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -4,11 +4,14 @@ import { Link } from "react-router-dom";
 function CityItem({ city }) {
   //
   // console.table(city);
-  const { cityName, emoji, date, id } = city;
+  const { cityName, emoji, date, id, position } = city;
 
   return (
     <li>
-      <Link className={styles.cityItem} to={`${id}`}>
+      <Link
+        className={styles.cityItem}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+      >
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
