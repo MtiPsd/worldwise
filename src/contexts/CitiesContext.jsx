dd63b351c1c8049acbfd8927f64191962b6be198
Mahdi Pasandideh <mtipsd.git@gmail.com>
2023-09-28 19:58:37 +0330

    refactored :: entire app to use context api

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 import { useEffect, useState } from "react";
 
 const BASE_URL = "http://localhost:8000";
@@ -37,4 +37,12 @@ function CitiesProvider({ children }) {
   );
 }
 
-export { CitiesProvider };
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined) {
+    throw Error("CitiesContext was used outside the CitiesProvider");
+  }
+  return context;
+}
+
+export { CitiesProvider, useCities };
